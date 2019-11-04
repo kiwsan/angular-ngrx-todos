f618cea8a38e6ef8a0c837d17adc8a2802453637
EKiwsant <kiwsanthia@gmail.com>
2019-11-04 22:34:27 +0700

    added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import {AppComponent} from './app.component';
+import {HelloComponent} from './hello.component';
+import {StoreModule} from '@ngrx/store';
+import {metaReducers, reducers} from './reducers';
+import {EffectsModule} from '@ngrx/effects';
+import {AppEffects} from './app.effects';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+    imports: [BrowserModule, FormsModule, StoreModule.forRoot(reducers, {
+        metaReducers,
+        runtimeChecks: {
+            strictStateImmutability: true,
+            strictActionImmutability: true
+        }
+    }), EffectsModule.forRoot([AppEffects])],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
