b85071a2d18e6ad0a16f03bfd0df4adc6a97f372
ekiwsant <ekkachai.kiwsanthia@aig.com>
2019-11-18 16:13:33 +0700

    added ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,60 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {CoreModule} from './core/core.module';
-import {SharedModule} from './shared/shared.module';
-import {StoreModule} from '@ngrx/store';
-import {metaReducers, reducers} from './reducers/app.state';
-import {EffectsModule} from '@ngrx/effects';
-import {AppEffects} from './app.effects';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CoreModule } from "./core/core.module";
+import { SharedModule } from "./shared/shared.module";
+import { StoreModule } from "@ngrx/store";
+import { metaReducers, reducers } from "./reducers/app.state";
+import { EffectsModule } from "@ngrx/effects";
+import { AppEffects } from "./app.effects";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(
+    http,
+    `${environment.i18nPrefix}/assets/i18n/`,
+    ".json"
+  );
+}
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        // vendor modules
-        BrowserModule,
-        // local modules
-        AppRoutingModule,
-        CoreModule,
-        SharedModule,
-        StoreModule.forRoot(reducers, {
-            metaReducers,
-            runtimeChecks: {
-                strictStateImmutability: true,
-                strictActionImmutability: true
-            }
-        }),
-        EffectsModule.forRoot([AppEffects]),
-        BrowserAnimationsModule,
-        FormsModule,
-        ReactiveFormsModule 
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [
+    // vendor modules
+    BrowserModule,
+    // local modules
+    AppRoutingModule,
+    CoreModule,
+    HttpClientModule,
+    SharedModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
+    EffectsModule.forRoot([AppEffects]),
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    // 3rd party
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
